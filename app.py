6d2fad7cbbfb5a86230da5c83235ea7f19232445
ken970054 <ken970054@gmail.com>
2021-11-04 20:48:20 +0800

    url_for update

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ def index_page():
                 doc_ref = db.collection(user_email).document("Record").collection(transType).document("YY" + editDate_split[0]).collection("MM" + editDate_split[1]).document("DD" + editDate_split[2] + "_" + str(editCount))
                 doc_ref.set(update_transfer_record, merge=True)
 
-            return redirect("/")
+            return redirect(url_for("/"))
 
         # 取得delete的post data
         if request.method == "POST" and "deleteButton" in request.get_json(): 
@@ -277,7 +277,7 @@ def index_page():
             doc_ref.delete() 
             #print(delete_item.to_dict())
             
-            return redirect("/")
+            return redirect(url_for("/"))
 
 
     except:
@@ -446,7 +446,7 @@ def account_book():
                     doc_ref = db.collection(user_email).document("Record").collection(transType).document("YY" + str(year_now)).collection("MM" + str(month_now)).document("DD" + str(day_now) + "_" + str(record_count))
                     doc_ref.set(transfer_quick_record)
 
-        return redirect("/account/book")
+        return redirect(url_for("/account/book"))
     
     # get data from edit accountBook record
     if request.method == "POST" and "editButton_book" in request.get_json():
@@ -493,7 +493,7 @@ def account_book():
             doc_ref = db.collection(user_email).document("Record").collection(transType).document("YY" + editDate_split[0]).collection("MM" + editDate_split[1]).document("DD" + editDate_split[2] + "_" + str(editCount))
             doc_ref.set(update_transfer_record, merge=True)
 
-        return redirect("/account/book")
+        return redirect(url_for("/account/book"))
 
     # get data from delete accountBook record
     if request.method == "POST" and "deleteButton_book" in request.get_json():
@@ -510,7 +510,7 @@ def account_book():
         doc_ref.delete() 
         #print(delete_item.to_dict())
         
-        return redirect("/account/book")
+        return redirect(url_for("/account/book"))
 
 
 @app.route('/account/manage', methods=['GET', 'POST'])
@@ -566,7 +566,7 @@ def account_manage():
         selected_account_name = request.get_json()['account_name']
         calculateTableContent(selected_account_name, originalDeposit, selected_year, Account, user_email)
 
-        return redirect("/account/manage")
+        return redirect(url_for("/account/manage"))
 
 @app.route('/account/setting', methods=['GET', 'POST'])
 def account_setting():
@@ -592,7 +592,7 @@ def account_setting():
         add_list = request.get_json()['add_list']
 
         modify_list(form_type, delete_list, add_list, year_now, user_email)
-        return redirect("/account/setting")
+        return redirect(url_for("/account/setting"))
     
 
 @app.route('/api/login', methods=['POST'])
